73c3b87a4107dd41bab1e6822fc18ac43bbd189d
muhrizky-a <muhrizky1337a@gmail.com>
2024-04-30 15:15:24 +0800

    forward&backward gait v2, other gait still testing

--- servo_driver_public_functions_v2.cpp
+++ servo_driver_public_functions_v2.cpp
@@ -167,12 +167,15 @@ void XServoDriverV2::forwardTripodGait() {
               
   
               // Set the new pulse width
+              
               _rightDriver.setPWM(0, 0, _angleToPulse(currentAngles_1[0]));
               _rightDriver.setPWM(1, 0, _angleToPulse(currentAngles_1[1]));
               _rightDriver.setPWM(2, 0, _angleToPulse(currentAngles_1[1]));
+              
               _rightDriver.setPWM(4, 0, _angleToPulse(currentAngles_2[0]));
               _rightDriver.setPWM(5, 0, _angleToPulse(currentAngles_2[1]));
               _rightDriver.setPWM(6, 0, _angleToPulse(currentAngles_2[1]));
+
               _rightDriver.setPWM(8, 0, _angleToPulse(currentAngles_1[0]));
               _rightDriver.setPWM(9, 0, _angleToPulse(currentAngles_1[1]));
               _rightDriver.setPWM(10, 0, _angleToPulse(currentAngles_1[1]));
@@ -180,9 +183,389 @@ void XServoDriverV2::forwardTripodGait() {
               _leftDriver.setPWM(0, 0, _angleToPulse(currentAngles_1[0]));
               _leftDriver.setPWM(1, 0, _angleToPulse(currentAngles_1[1]));
               _leftDriver.setPWM(2, 0, _angleToPulse(currentAngles_1[1]));
+              
               _leftDriver.setPWM(4, 0, _angleToPulse(currentAngles_2[0]));
               _leftDriver.setPWM(5, 0, _angleToPulse(currentAngles_2[1]));
               _leftDriver.setPWM(6, 0, _angleToPulse(currentAngles_2[1]));
+
+              _leftDriver.setPWM(8, 0, _angleToPulse(currentAngles_1[0]));
+              _leftDriver.setPWM(9, 0, _angleToPulse(currentAngles_1[1]));
+              _leftDriver.setPWM(10, 0, _angleToPulse(currentAngles_1[1])); 
+          }
+      }
+      // Set the final position;
+      Serial.println("");
+      Serial.println("=final=");
+//      Serial.println(currentAngles_1[0]);
+//      delay(1000);
+    }
+
+    // Switch lifted leg and repeat
+//    XliftedLeg = (XliftedLeg + 1) % XnumLegs;
+}
+
+void XServoDriverV2::backwardTripodGait() {
+  Serial.print("STARTO ");
+  int steps = Xduration / XstepInterval;  // Assuming interval ms per step
+  
+    const int targetAngles_1[4][2] = {
+      {90,LIFTED_RIGHT_LEG_ANGLE},
+      {110,STAND_ANGLE},
+      {90,STAND_ANGLE},
+      {70,STAND_ANGLE},
+    };
+
+    const int targetAngles_2[4][2] = {
+      {90,STAND_ANGLE},
+      {70,STAND_ANGLE},
+      {90,LIFTED_RIGHT_LEG_ANGLE},
+      {110,STAND_ANGLE},
+    };
+    
+//    currentAngles[0] = targetAngles[0][0];
+//    Serial.println(currentAngles[0]);
+//    currentAngles[1] = targetAngles[0][1];  
+    
+    // Gerak
+    for(int i=0; i < 4; i++){
+      unsigned long startTime = millis();
+      
+      
+//      Serial.print("current angle begin: ");
+//      Serial.println(currentAngles[1]);
+//      Serial.print("target angle begin: ");
+//      Serial.println(targetAngles[i][1]); 
+//
+      Serial.print("inc beginz: ");
+      Serial.print(" ");
+      increments_1[0] = targetAngles_1[i][0] - currentAngles_1[0];
+      increments_1[1] = targetAngles_1[i][1] - currentAngles_1[1];
+      increments_2[0] = targetAngles_2[i][0] - currentAngles_2[0];
+      increments_2[1] = targetAngles_2[i][1] - currentAngles_2[1];
+//      Serial.print(increments[0]);
+//      Serial.print(" ");
+//      Serial.print(steps);
+//      Serial.print(" ");
+      increments_1[0] = abs(increments_1[0]) / steps;
+      increments_1[1] = abs(increments_1[1]) / steps;
+      increments_2[0] = abs(increments_2[0]) / steps;
+      increments_2[1] = abs(increments_2[1]) / steps;
+//      increments[0] = abs(increments[0]);
+//      Serial.print(" ");
+//      Serial.println(increments_1[0]);
+      
+      // Count to Xduration (0-1000)
+      Serial.print("inc-ed angles: ");
+      while (millis() - startTime <= Xduration) {
+          // Check if it's time to move to the next step (XstepInterval is 20, so stepping 0,20,40,80,...,1000)
+          
+          if (millis() - XpreviousMillis >= XstepInterval) {
+
+              // Update the previousMillis for the next step
+              XpreviousMillis = millis();
+              
+              if(targetAngles_1[i][0] < currentAngles_1[0]){
+                currentAngles_1[0] -= increments_1[0];
+              } else if(targetAngles_1[i][0] > currentAngles_1[0]) {
+                currentAngles_1[0] += increments_1[0];
+              }
+              if(targetAngles_1[i][1] < currentAngles_1[1]){
+                currentAngles_1[1] -= increments_1[1];
+              } else if(targetAngles_1[i][1] > currentAngles_1[1]) {
+                currentAngles_1[1] += increments_1[1];
+              }
+
+              if(targetAngles_2[i][0] < currentAngles_2[0]){
+                currentAngles_2[0] -= increments_2[0];
+              } else if(targetAngles_2[i][0] > currentAngles_2[0]) {
+                currentAngles_2[0] += increments_2[0];
+              }
+
+              if(targetAngles_2[i][1] < currentAngles_2[1]){
+                currentAngles_2[1] -= increments_2[1];
+              } else if(targetAngles_2[i][1] > currentAngles_2[1]) {
+                currentAngles_2[1] += increments_2[1];
+              }
+
+//              Serial.print(currentAngles[1]);
+//              Serial.print(" ");
+              
+  
+              // Set the new pulse width
+              
+              _rightDriver.setPWM(0, 0, _angleToPulse(currentAngles_1[0]));
+              _rightDriver.setPWM(1, 0, _angleToPulse(currentAngles_1[1]));
+              _rightDriver.setPWM(2, 0, _angleToPulse(currentAngles_1[1]));
+              
+              _rightDriver.setPWM(4, 0, _angleToPulse(currentAngles_2[0]));
+              _rightDriver.setPWM(5, 0, _angleToPulse(currentAngles_2[1]));
+              _rightDriver.setPWM(6, 0, _angleToPulse(currentAngles_2[1]));
+
+              _rightDriver.setPWM(8, 0, _angleToPulse(currentAngles_1[0]));
+              _rightDriver.setPWM(9, 0, _angleToPulse(currentAngles_1[1]));
+              _rightDriver.setPWM(10, 0, _angleToPulse(currentAngles_1[1]));
+
+//              ---
+              _leftDriver.setPWM(0, 0, _angleToPulse(currentAngles_1[0]));
+              _leftDriver.setPWM(1, 0, _angleToPulse(currentAngles_1[1]));
+              _leftDriver.setPWM(2, 0, _angleToPulse(currentAngles_1[1]));
+              
+              _leftDriver.setPWM(4, 0, _angleToPulse(currentAngles_2[0]));
+              _leftDriver.setPWM(5, 0, _angleToPulse(currentAngles_2[1]));
+              _leftDriver.setPWM(6, 0, _angleToPulse(currentAngles_2[1]));
+
+              _leftDriver.setPWM(8, 0, _angleToPulse(currentAngles_1[0]));
+              _leftDriver.setPWM(9, 0, _angleToPulse(currentAngles_1[1]));
+              _leftDriver.setPWM(10, 0, _angleToPulse(currentAngles_1[1])); 
+          }
+      }
+      // Set the final position;
+      Serial.println("");
+      Serial.println("=final=");
+//      Serial.println(currentAngles_1[0]);
+//      delay(1000);
+    }
+
+    // Switch lifted leg and repeat
+//    XliftedLeg = (XliftedLeg + 1) % XnumLegs;
+}
+
+void XServoDriverV2::turnLeftTripodGait() {
+  Serial.print("STARTO ");
+  int steps = Xduration / XstepInterval;  // Assuming interval ms per step
+  
+    const int targetAngles_1[4][2] = {
+      {90,LIFTED_RIGHT_LEG_ANGLE},
+      {70,STAND_ANGLE},
+      {90,STAND_ANGLE},
+      {110,STAND_ANGLE},
+    };
+
+    const int targetAngles_2[4][2] = {
+      {90,STAND_ANGLE},
+      {70,STAND_ANGLE},
+      {90,LIFTED_RIGHT_LEG_ANGLE},
+      {110,STAND_ANGLE},
+    };
+    
+//    currentAngles[0] = targetAngles[0][0];
+//    Serial.println(currentAngles[0]);
+//    currentAngles[1] = targetAngles[0][1];  
+    
+    // Gerak
+    for(int i=0; i < 4; i++){
+      unsigned long startTime = millis();
+      
+      
+//      Serial.print("current angle begin: ");
+//      Serial.println(currentAngles[1]);
+//      Serial.print("target angle begin: ");
+//      Serial.println(targetAngles[i][1]); 
+//
+      Serial.print("inc beginz: ");
+      Serial.print(" ");
+      increments_1[0] = targetAngles_1[i][0] - currentAngles_1[0];
+      increments_1[1] = targetAngles_1[i][1] - currentAngles_1[1];
+      increments_2[0] = targetAngles_2[i][0] - currentAngles_2[0];
+      increments_2[1] = targetAngles_2[i][1] - currentAngles_2[1];
+//      Serial.print(increments[0]);
+//      Serial.print(" ");
+//      Serial.print(steps);
+//      Serial.print(" ");
+      increments_1[0] = abs(increments_1[0]) / steps;
+      increments_1[1] = abs(increments_1[1]) / steps;
+      increments_2[0] = abs(increments_2[0]) / steps;
+      increments_2[1] = abs(increments_2[1]) / steps;
+//      increments[0] = abs(increments[0]);
+//      Serial.print(" ");
+//      Serial.println(increments_1[0]);
+      
+      // Count to Xduration (0-1000)
+      Serial.print("inc-ed angles: ");
+      while (millis() - startTime <= Xduration) {
+          // Check if it's time to move to the next step (XstepInterval is 20, so stepping 0,20,40,80,...,1000)
+          
+          if (millis() - XpreviousMillis >= XstepInterval) {
+
+              // Update the previousMillis for the next step
+              XpreviousMillis = millis();
+              
+              if(targetAngles_1[i][0] < currentAngles_1[0]){
+                currentAngles_1[0] -= increments_1[0];
+              } else if(targetAngles_1[i][0] > currentAngles_1[0]) {
+                currentAngles_1[0] += increments_1[0];
+              }
+              if(targetAngles_1[i][1] < currentAngles_1[1]){
+                currentAngles_1[1] -= increments_1[1];
+              } else if(targetAngles_1[i][1] > currentAngles_1[1]) {
+                currentAngles_1[1] += increments_1[1];
+              }
+
+              if(targetAngles_2[i][0] < currentAngles_2[0]){
+                currentAngles_2[0] -= increments_2[0];
+              } else if(targetAngles_2[i][0] > currentAngles_2[0]) {
+                currentAngles_2[0] += increments_2[0];
+              }
+
+              if(targetAngles_2[i][1] < currentAngles_2[1]){
+                currentAngles_2[1] -= increments_2[1];
+              } else if(targetAngles_2[i][1] > currentAngles_2[1]) {
+                currentAngles_2[1] += increments_2[1];
+              }
+
+//              Serial.print(currentAngles[1]);
+//              Serial.print(" ");
+              
+  
+              // Set the new pulse width
+              
+              _rightDriver.setPWM(0, 0, _angleToPulse(currentAngles_1[0]));
+              _rightDriver.setPWM(1, 0, _angleToPulse(currentAngles_1[1]));
+              _rightDriver.setPWM(2, 0, _angleToPulse(currentAngles_1[1]));
+              
+              _rightDriver.setPWM(4, 0, _angleToPulse(currentAngles_2[0]));
+              _rightDriver.setPWM(5, 0, _angleToPulse(currentAngles_2[1]));
+              _rightDriver.setPWM(6, 0, _angleToPulse(currentAngles_2[1]));
+
+              _rightDriver.setPWM(8, 0, _angleToPulse(currentAngles_1[0]));
+              _rightDriver.setPWM(9, 0, _angleToPulse(currentAngles_1[1]));
+              _rightDriver.setPWM(10, 0, _angleToPulse(currentAngles_1[1]));
+
+//              ---
+              _leftDriver.setPWM(0, 0, _angleToPulse(currentAngles_1[0]));
+              _leftDriver.setPWM(1, 0, _angleToPulse(currentAngles_1[1]));
+              _leftDriver.setPWM(2, 0, _angleToPulse(currentAngles_1[1]));
+              
+              _leftDriver.setPWM(4, 0, _angleToPulse(currentAngles_2[0]));
+              _leftDriver.setPWM(5, 0, _angleToPulse(currentAngles_2[1]));
+              _leftDriver.setPWM(6, 0, _angleToPulse(currentAngles_2[1]));
+
+              _leftDriver.setPWM(8, 0, _angleToPulse(currentAngles_1[0]));
+              _leftDriver.setPWM(9, 0, _angleToPulse(currentAngles_1[1]));
+              _leftDriver.setPWM(10, 0, _angleToPulse(currentAngles_1[1])); 
+          }
+      }
+      // Set the final position;
+      Serial.println("");
+      Serial.println("=final=");
+//      Serial.println(currentAngles_1[0]);
+//      delay(1000);
+    }
+
+    // Switch lifted leg and repeat
+//    XliftedLeg = (XliftedLeg + 1) % XnumLegs;
+}
+
+void XServoDriverV2::turnRightTripodGait() {
+  Serial.print("STARTO ");
+  int steps = Xduration / XstepInterval;  // Assuming interval ms per step
+  
+    const int targetAngles_1[4][2] = {
+      {90,LIFTED_RIGHT_LEG_ANGLE},
+      {110,STAND_ANGLE},
+      {90,STAND_ANGLE},
+      {70,STAND_ANGLE},
+    };
+
+    const int targetAngles_2[4][2] = {
+      {90,STAND_ANGLE},
+      {110,STAND_ANGLE},
+      {90,LIFTED_RIGHT_LEG_ANGLE},
+      {70,STAND_ANGLE},
+    };
+    
+//    currentAngles[0] = targetAngles[0][0];
+//    Serial.println(currentAngles[0]);
+//    currentAngles[1] = targetAngles[0][1];  
+    
+    // Gerak
+    for(int i=0; i < 4; i++){
+      unsigned long startTime = millis();
+      
+      
+//      Serial.print("current angle begin: ");
+//      Serial.println(currentAngles[1]);
+//      Serial.print("target angle begin: ");
+//      Serial.println(targetAngles[i][1]); 
+//
+      Serial.print("inc beginz: ");
+      Serial.print(" ");
+      increments_1[0] = targetAngles_1[i][0] - currentAngles_1[0];
+      increments_1[1] = targetAngles_1[i][1] - currentAngles_1[1];
+      increments_2[0] = targetAngles_2[i][0] - currentAngles_2[0];
+      increments_2[1] = targetAngles_2[i][1] - currentAngles_2[1];
+//      Serial.print(increments[0]);
+//      Serial.print(" ");
+//      Serial.print(steps);
+//      Serial.print(" ");
+      increments_1[0] = abs(increments_1[0]) / steps;
+      increments_1[1] = abs(increments_1[1]) / steps;
+      increments_2[0] = abs(increments_2[0]) / steps;
+      increments_2[1] = abs(increments_2[1]) / steps;
+//      increments[0] = abs(increments[0]);
+//      Serial.print(" ");
+//      Serial.println(increments_1[0]);
+      
+      // Count to Xduration (0-1000)
+      Serial.print("inc-ed angles: ");
+      while (millis() - startTime <= Xduration) {
+          // Check if it's time to move to the next step (XstepInterval is 20, so stepping 0,20,40,80,...,1000)
+          
+          if (millis() - XpreviousMillis >= XstepInterval) {
+
+              // Update the previousMillis for the next step
+              XpreviousMillis = millis();
+              
+              if(targetAngles_1[i][0] < currentAngles_1[0]){
+                currentAngles_1[0] -= increments_1[0];
+              } else if(targetAngles_1[i][0] > currentAngles_1[0]) {
+                currentAngles_1[0] += increments_1[0];
+              }
+              if(targetAngles_1[i][1] < currentAngles_1[1]){
+                currentAngles_1[1] -= increments_1[1];
+              } else if(targetAngles_1[i][1] > currentAngles_1[1]) {
+                currentAngles_1[1] += increments_1[1];
+              }
+
+              if(targetAngles_2[i][0] < currentAngles_2[0]){
+                currentAngles_2[0] -= increments_2[0];
+              } else if(targetAngles_2[i][0] > currentAngles_2[0]) {
+                currentAngles_2[0] += increments_2[0];
+              }
+
+              if(targetAngles_2[i][1] < currentAngles_2[1]){
+                currentAngles_2[1] -= increments_2[1];
+              } else if(targetAngles_2[i][1] > currentAngles_2[1]) {
+                currentAngles_2[1] += increments_2[1];
+              }
+
+//              Serial.print(currentAngles[1]);
+//              Serial.print(" ");
+              
+  
+              // Set the new pulse width
+              
+              _rightDriver.setPWM(0, 0, _angleToPulse(currentAngles_1[0]));
+              _rightDriver.setPWM(1, 0, _angleToPulse(currentAngles_1[1]));
+              _rightDriver.setPWM(2, 0, _angleToPulse(currentAngles_1[1]));
+              
+              _rightDriver.setPWM(4, 0, _angleToPulse(currentAngles_2[0]));
+              _rightDriver.setPWM(5, 0, _angleToPulse(currentAngles_2[1]));
+              _rightDriver.setPWM(6, 0, _angleToPulse(currentAngles_2[1]));
+
+              _rightDriver.setPWM(8, 0, _angleToPulse(currentAngles_1[0]));
+              _rightDriver.setPWM(9, 0, _angleToPulse(currentAngles_1[1]));
+              _rightDriver.setPWM(10, 0, _angleToPulse(currentAngles_1[1]));
+
+//              ---
+              _leftDriver.setPWM(0, 0, _angleToPulse(currentAngles_1[0]));
+              _leftDriver.setPWM(1, 0, _angleToPulse(currentAngles_1[1]));
+              _leftDriver.setPWM(2, 0, _angleToPulse(currentAngles_1[1]));
+              
+              _leftDriver.setPWM(4, 0, _angleToPulse(currentAngles_2[0]));
+              _leftDriver.setPWM(5, 0, _angleToPulse(currentAngles_2[1]));
+              _leftDriver.setPWM(6, 0, _angleToPulse(currentAngles_2[1]));
+
               _leftDriver.setPWM(8, 0, _angleToPulse(currentAngles_1[0]));
               _leftDriver.setPWM(9, 0, _angleToPulse(currentAngles_1[1]));
               _leftDriver.setPWM(10, 0, _angleToPulse(currentAngles_1[1])); 
